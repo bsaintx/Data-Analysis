426d1011e8a9ea0e4116c86e809a51333d5e3328
Bruno Coutinho <111131598+bsaintx@users.noreply.github.com>
2022-09-25 14:21:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 table = pd.read_excel("sales.xlsx")
 
-#  Overview of total billing
-total_billing = table["Valor Final"].sum()
+#   Overview of total sales
+total_sales = table["Final Price"].sum()
 
-# 
+#   Company Sales - Top -> Down
+company_sales = table[["Company", "Final Price"]].groupby("Company").sum()
+
+# Top selling products
+top_selling = table[["Company", "Final Price"]].groupby(["Company", "Product"]).sum()
